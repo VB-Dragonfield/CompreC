bc8c1ec86de24d8780a21730cd7cc9d0525d2379
VB-Dragonfield <122823675+VB-Dragonfield@users.noreply.github.com>
2023-06-19 12:52:49 +0200

    Create Extract_fich.c
    Création de la fonction d'extraction de fichier d'une archive dans un répertoire défini.

---
+++ Extract_fich.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <zip.h>
+
+int extraireFichierArchive(struct zip* archive, const char* cheminFichier, const char* cheminDestination)
+{
+    struct zip_file* fichier = zip_fopen(archive, cheminFichier, 0);
+    if (!fichier)
+    {
+        printf("Impossible d'ouvrir le fichier : %s\n", cheminFichier);
+        return -1;
+    }
+
+    FILE* fichierDestination = fopen(cheminDestination, "wb");
+    if (!fichierDestination)
+    {
+        printf("Impossible de créer le fichier de destination : %s\n", cheminDestination);
+        zip_fclose(fichier);
+        return -1;
+    }
+
+    char buffer[1024];
+    int bytesRead;
+    while ((bytesRead = zip_fread(fichier, buffer, sizeof(buffer))) > 0)
+    {
+        fwrite(buffer, 1, bytesRead, fichierDestination);
+    }
+
+    fclose(fichierDestination);
+    zip_fclose(fichier);
+
+    printf("Extraction terminée : %s extrait vers %s\n", cheminFichier, cheminDestination);
+    return 0;
+}
