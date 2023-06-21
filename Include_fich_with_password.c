d405d52f0f3b256fac679bc90ad6eecd8c618421
VB-Dragonfield <122823675+VB-Dragonfield@users.noreply.github.com>
2023-06-21 10:17:26 +0200

    Create Include_fich_with_password.c
    Création de la fonction d'inclusion d'un fichier dans une archive chiffré.

---
+++ Include_fich_with_password.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <zip.h>
+
+int inclureFichierArchive(struct zip* archive, const char* cheminFichier, const char* cheminDestination)
+{
+    FILE* fichier = fopen(cheminFichier, "rb");
+    if (!fichier)
+    {
+        printf("Impossible d'ouvrir le fichier : %s\n", cheminFichier);
+        return -1;
+    }
+
+    fseek(fichier, 0, SEEK_END);
+    long tailleFichier = ftell(fichier);
+    fseek(fichier, 0, SEEK_SET);
+
+    struct zip_source* source = zip_source_file(archive, cheminFichier, 0, -1);
+    if (!source)
+    {
+        printf("Impossible de créer la source pour le fichier : %s\n", cheminFichier);
+        fclose(fichier);
+        return -1;
+    }
+
+    struct zip_file* fichierDestination = zip_fopen(archive, cheminDestination, ZIP_FL_OVERWRITE);
+    if (!fichierDestination)
+    {
+        printf("Impossible de créer le fichier de destination dans l'archive : %s\n", cheminDestination);
+        zip_source_free(source);
+        fclose(fichier);
+        return -1;
+    }
+
+    char buffer[1024];
+    int bytesRead;
+    while ((bytesRead = fread(buffer, 1, sizeof(buffer), fichier)) > 0)
+    {
+        zip_fwrite(fichierDestination, buffer, bytesRead);
+    }
+
+    fclose(fichier);
+    zip_fclose(fichierDestination);
+    zip_source_free(source);
+
+    printf("Inclusion terminée : %s inclus dans l'archive avec le chemin : %s\n", cheminFichier, cheminDestination);
+    return 0;
+}
