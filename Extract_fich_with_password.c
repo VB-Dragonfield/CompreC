0540c808e24ce56ac17e660631418ea74f455895
VB-Dragonfield <122823675+VB-Dragonfield@users.noreply.github.com>
2023-06-21 10:20:57 +0200

    Update Extract_fich_with_password.c
    Ajustement de la position de la définition du mot de passe.

--- Extract_fich_with_password.c
+++ Extract_fich_with_password.c
@@ -4,16 +4,16 @@
 
 int extraireFichierArchive(struct zip* archive, const char* cheminFichier, const char* cheminDestination)
 {
-    // Définir le mot de passe par défaut
-    zip_set_default_password(archive, password);    
-  
     struct zip_file* fichier = zip_fopen(archive, cheminFichier, 0);
     if (!fichier)
     {
         printf("Impossible d'ouvrir le fichier : %s\n", cheminFichier);
         return -1;
     }
-
+    
+    // Définir le mot de passe par défaut
+    zip_set_default_password(archive, password);   
+    
     FILE* fichierDestination = fopen(cheminDestination, "wb");
     if (!fichierDestination)
     {
