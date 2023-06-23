48f5d1ae11dddbbd5555322ce54c8d889940927a
VB-Dragonfield <122823675+VB-Dragonfield@users.noreply.github.com>
2023-06-23 12:51:56 +0200

    Update Bruteforce_dictionnary.c
    Ajout des commentaires

--- Bruteforce_dictionnary.c
+++ Bruteforce_dictionnary.c
@@ -3,48 +3,72 @@
 #include <string.h>
 #include <zip.h>
 
-int bruteForceZipPasswordDictionnary(const char* archivePath, const char* dictionaryPath) {
+int bruteForceZipPassword(const char* archivePath, const char* charset, int maxLength) {
+    int charsetLength = strlen(charset); // Longueur de l'ensemble de caractères possibles pour le mot de passe
+    int* indices = (int*)malloc(maxLength * sizeof(int)); // Tableau d'indices pour générer les combinaisons de caractères
 
-
-    struct zip* archive = zip_open(archivePath, 0, 'r');
+    struct zip* archive = zip_open(archivePath, 0, 'r'); // Ouverture de l'archive en mode lecture seule
     if (!archive) {
-        printf("Erreur lors de l'ouverture de l'archive.\n");
+        printf("Erreur lors de l'ouverture de l'archive.\n"); // Affiche une erreur si l'ouverture de l'archive échoue
         return -1;
     }
 
-    FILE* dictionary = fopen(dictionaryPath, "r");
-    if (!dictionary) {
-        printf("Erreur lors de l'ouverture du dictionnaire.\n");
-        zip_close(archive);
-        return -1;
-    }
+    int foundPassword = 0; // Indicateur indiquant si le mot de passe a été trouvé
+    int passwordLength = 1; // Longueur initiale du mot de passe à tester
 
-    int foundPassword = 0;
-    char password[256];
+    while (passwordLength <= maxLength) { // Boucle pour tester les mots de passe de différentes longueurs
+        int i;
+        for (i = 0; i < passwordLength; i++) {
+            indices[i] = 0; // Initialise tous les indices à 0 pour générer la première combinaison
+        }
 
-    while (fgets(password, sizeof(password), dictionary)) {
-        int passwordLength = strlen(password);
-        if (password[passwordLength - 1] == '\n')
-            password[--passwordLength] = '\0';
+        char* password = (char*)malloc((passwordLength + 1) * sizeof(char)); // Alloue de la mémoire pour le mot de passe à tester
+        password[passwordLength] = '\0'; // Ajoute le caractère de fin de chaîne
 
-        if (passwordLength <= maxLength) {
-            int result = zip_set_default_password(archive, password);
+        while (1) { // Boucle pour générer et tester toutes les combinaisons possibles de caractères
+            for (i = 0; i < passwordLength; i++) {
+                password[i] = charset[indices[i]]; // Construit le mot de passe en utilisant les caractères correspondants aux indices
+            }
+
+            int result = zip_set_default_password(archive, password); // Définit le mot de passe par défaut pour l'archive
             if (result == 0) {
-                printf("Mot de passe trouvé : %s\n", password);
+                printf("Mot de passe trouvé : %s\n", password); // Affiche le mot de passe trouvé
                 foundPassword = 1;
                 break;
             }
+
+            int carry = 1;
+            for (i = passwordLength - 1; i >= 0; i--) {
+                indices[i] += carry; // Incrémente l'indice actuel
+                if (indices[i] == charsetLength) { // Vérifie si l'indice dépasse la taille de l'ensemble de caractères
+                    indices[i] = 0; // Réinitialise l'indice à 0
+                    carry = 1; // Propage la retenue
+                } else {
+                    carry = 0; // Pas de retenue, arrête l'incrémentation
+                    break;
+                }
+            }
+
+            if (carry)
+                break; // Si la retenue est propagée jusqu'à la première position, toutes les combinaisons ont été testées
+
         }
+
+        free(password); // Libère la mémoire allouée pour le mot de passe
+
+        if (foundPassword)
+            break; // Si le mot de passe a été trouvé, arrête la boucle
+
+        passwordLength++; // Incrémente la longueur du mot de passe à tester
     }
 
-    fclose(dictionary);
-    zip_close(archive);
-    free(indices);
+    zip_close(archive); // Ferme l'archive
+    free(indices); // Libère la mémoire allouée pour les indices
 
     if (!foundPassword) {
-        printf("Mot de passe introuvable.\n");
+        printf("Mot de passe introuvable.\n"); // Affiche un message si le mot de passe n'a pas été trouvé
         return -1;
     }
 
-    return 0;
+    return 0; // Retourne 0 pour indiquer que le processus s'est terminé avec succès
 }
