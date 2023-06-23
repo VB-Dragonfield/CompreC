872d05da7a9acbabd7b6aa8e95ac28361b49433b
VB-Dragonfield <122823675+VB-Dragonfield@users.noreply.github.com>
2023-06-23 13:02:10 +0200

    Update Extract_fich_with_password.c
    Ajout des commentaires

--- Extract_fich_with_password.c
+++ Extract_fich_with_password.c
@@ -4,44 +4,40 @@
 
 int extraireFichierArchivePassword(const char* cheminArchive, const char* cheminFichier, const char* cheminDestination, const char* password)
 {
-    struct zip* archive = zip_open(cheminArchive, 0, NULL);
+    struct zip* archive = zip_open(cheminArchive, 0, NULL); // Ouverture de l'archive ZIP spécifiée par le chemin
     if (!archive)
     {
-        printf("Impossible d'ouvrir l'archive : %s\n", cheminArchive);
-        return;
-    }
-    
-    // Définir le mot de passe par défaut
-    zip_set_default_password(archive, password);
-    
-    struct zip_file* fichier = zip_fopen(archive, cheminFichier, 0);
-    if (!fichier)
-    {
-        printf("Impossible d'ouvrir le fichier : %s\n", cheminFichier);
+        printf("Impossible d'ouvrir l'archive : %s\n", cheminArchive); // Affiche un message d'erreur si l'ouverture de l'archive échoue
         return -1;
     }
     
-    // Définir le mot de passe par défaut
-    zip_set_default_password(archive, password);   
+    zip_set_default_password(archive, password); // Définition du mot de passe par défaut pour l'archive
     
-    FILE* fichierDestination = fopen(cheminDestination, "wb");
+    struct zip_file* fichier = zip_fopen(archive, cheminFichier, 0); // Ouverture du fichier spécifié dans l'archive
+    if (!fichier)
+    {
+        printf("Impossible d'ouvrir le fichier : %s\n", cheminFichier); // Affiche un message d'erreur si l'ouverture du fichier échoue
+        return -1;
+    }
+    
+    FILE* fichierDestination = fopen(cheminDestination, "wb"); // Création du fichier de destination en mode binaire
     if (!fichierDestination)
     {
-        printf("Impossible de créer le fichier de destination : %s\n", cheminDestination);
+        printf("Impossible de créer le fichier de destination : %s\n", cheminDestination); // Affiche un message d'erreur si la création du fichier de destination échoue
         zip_fclose(fichier);
         return -1;
     }
 
-    char buffer[1024];
+    char buffer[1024]; // Tampon pour lire les données du fichier
     int bytesRead;
-    while ((bytesRead = zip_fread(fichier, buffer, sizeof(buffer))) > 0)
+    while ((bytesRead = zip_fread(fichier, buffer, sizeof(buffer))) > 0) // Lecture du fichier dans des blocs de taille fixe
     {
-        fwrite(buffer, 1, bytesRead, fichierDestination);
+        fwrite(buffer, 1, bytesRead, fichierDestination); // Écriture des données lues dans le fichier de destination
     }
 
-    fclose(fichierDestination);
-    zip_fclose(fichier);
+    fclose(fichierDestination); // Fermeture du fichier de destination
+    zip_fclose(fichier); // Fermeture du fichier dans l'archive
 
-    printf("Extraction terminée : %s extrait vers %s\n", cheminFichier, cheminDestination);
+    printf("Extraction terminée : %s extrait vers %s\n", cheminFichier, cheminDestination); // Affiche un message de confirmation de l'extraction
     return 0;
 }
